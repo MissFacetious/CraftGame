41a01eb3e637cc473fb29c894dbba8fef00f6feb
pharrowant <me@pharrow.net>
2020-09-07 02:52:15 -0400

    Update Tree.cs

--- Assets/Scripts/Tree.cs
+++ Assets/Scripts/Tree.cs
@@ -8,10 +8,15 @@ public class Tree : MonoBehaviour
     public bool showButton;
     public GameObject Box;
     public GameObject Sphere;
+
+    [SerializeField]
+    private bool hasItemsToDrop;
+
     // Start is called before the first frame update
     void Start()
     {
         showButton = false;
+        hasItemsToDrop = false;
     }
 
     // Update is called once per frame
@@ -22,17 +27,41 @@ public class Tree : MonoBehaviour
             Box.SetActive(true);
             if (Keyboard.current.aKey.wasPressedThisFrame)
             {
-                Destroy(GetComponent<Collider>());
-                showButton = false;
-                // create a shower of other items
-                for (int i = 0; i < 10; i++) {
-                    GameObject branch = Instantiate(Sphere, transform.position + new Vector3(0f, 1f, 0f), Quaternion.identity) as GameObject;
-                }
+                //Destroy(GetComponent<Collider>());
+                //showButton = false;
+                //// create a shower of other items
+                //for (int i = 0; i < 10; i++) {
+                //    GameObject branch = Instantiate(Sphere, transform.position + new Vector3(0f, 1f, 0f), Quaternion.identity) as GameObject;
+                //}
             }
         }
         else
         {
             Box.SetActive(false);
+        }
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        
+    }
+
+    public void Interaction()
+    {
+        ShowerItems();
+    }
+
+    private void ShowerItems()
+    {
+        if (hasItemsToDrop)
+        {
+            //showButton = false;
+            //for (int i = 0; i < objectPool.Count; i++)
+            //{
+            //    GameObject obj = objectPool.
+            //}
+
+            hasItemsToDrop = false;
         }
     }
 }
