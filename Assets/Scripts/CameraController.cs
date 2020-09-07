4fed0f4093bb711206fced53c5c48058c3091d0f
pharrowant <me@pharrow.net>
2020-09-07 02:54:37 -0400

    Added Camera Controller
    Camera can now be controlled by mouse and right joystick. Added wonky collsion, Pokecam, and X/Y axis inversion.

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,211 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.Internal;
+
+public class CameraController : MonoBehaviour
+{
+    // TODO (aoyeola): Unserialize private fields once ranges are finalized;
+    [SerializeField]
+    public GameObject target = default;
+    
+    [HideInInspector]
+    public Vector2 lookVector;
+
+    private Camera regularCamera;
+    private Vector2 cameraOrbitAngle;
+    private Vector3 focusPoint;
+    private Quaternion lookRotation;
+    
+
+    [SerializeField, Range(1f, 20f)]
+    private float cameraDistance = 6f;
+
+    [SerializeField, Range(1f, 25f)]
+    private float cameraPanRadius = 2f;
+
+    [SerializeField, Min(0f)]
+    private float cameraResetDelay = 5f;
+
+    [SerializeField, Range(0f, 1f)]
+    private float cameraResetSpeed = 0.75f;
+
+    [SerializeField, Range(1f, 100f)]
+    private float cameraRotateSpeed = 30f;
+
+    [SerializeField, Range(-15f, 80f)]
+    private float cameraMinYAngle = -5f, cameraMaxYAngle = 45f;
+
+    [SerializeField, Range(0f, 1f)]
+    private float cameraSensitivity = 0.4f;
+
+    // NOTE (aoyeola): Useful for locking camera pos during cinematic events
+    [SerializeField]
+    private bool canMove = true;
+
+    // TODO (aoyeola): Expose these flags in an Options menu
+    [SerializeField]
+    private bool invertXAxis, invertYAxis;
+
+    [SerializeField]
+    private LayerMask clippingMask = -1;
+
+    public Vector3 CameraHalf
+    {
+        get
+        {
+            Vector3 halfExtends;
+            halfExtends.y = regularCamera.nearClipPlane * Mathf.Tan(0.5f * Mathf.Deg2Rad * regularCamera.fieldOfView);
+            halfExtends.x = halfExtends.y * regularCamera.aspect;
+            halfExtends.z = 0f;
+            return halfExtends;
+        }
+    }
+
+
+    private void Awake()
+    {
+        focusPoint = target.transform.position;
+        cameraOrbitAngle = new Vector2(45f, 0f);
+        lookVector = Vector2.zero;
+        transform.localRotation = Quaternion.Euler(cameraOrbitAngle);
+        regularCamera = GetComponent<Camera>();
+
+        // NOTE (aoyeola): Only for debugging/playtesting
+        // Forced Pokemon style stationary camera
+        if (canMove.Equals(false))
+        {
+            cameraDistance = 8f;
+            cameraPanRadius = 2f;
+            cameraResetSpeed = 0.9f;
+        }
+    }
+
+    private void LateUpdate()
+    {
+        UpdateCameraInput();
+        UpdateCameraTracking();
+        //UpdateCameraInput();
+
+        Vector3 lookDir = lookRotation * Vector3.forward;
+        Vector3 lookPosition = focusPoint - lookDir * cameraDistance;
+
+        Vector3 rectOffset = lookDir * regularCamera.nearClipPlane;
+        Vector3 rectPosition = lookPosition + rectOffset;
+        Vector3 castFrom = target.transform.position;
+        Vector3 castLine = rectPosition - castFrom;
+        float castDistance = castLine.magnitude;
+        Vector3 castDirection = castLine / castDistance;
+
+        // camera collision - cast a box from camera to the target. If a raycast hit is detected, 
+        // reposition the camera within distance to the near clipping plane
+        if (false && Physics.Raycast(focusPoint, -lookDir, out RaycastHit hit, cameraDistance))
+        {
+            lookPosition = focusPoint - lookDir * hit.distance;
+        }
+
+        if (Physics.BoxCast(focusPoint, CameraHalf, castDirection, out RaycastHit hitinfo, lookRotation, castDistance, clippingMask))
+        {
+            rectPosition = castFrom + castDirection * hitinfo.distance;
+            lookPosition = rectPosition - rectOffset;
+        }
+        transform.SetPositionAndRotation(lookPosition, lookRotation);
+    }
+
+    private void OnValidate()
+    {
+        if (cameraMaxYAngle < cameraMinYAngle)
+        {
+            cameraMaxYAngle = cameraMinYAngle;
+        }
+    }
+
+    private void UpdateCameraTracking()
+    {
+        Vector3 targetPosition = target.transform.position;
+
+        if (cameraPanRadius > 0f)
+        {
+            // Recenter the camera on target logarithmically (instead of linearly)
+            // s.t. distance is halved each time. Default is 0.5 but can be
+            // any arbitrary scalar.
+            float targetDist = Vector3.Distance(targetPosition, focusPoint);
+            float t = 1f;
+            if ((targetDist >= 0.01f) && (cameraResetSpeed > 0f))
+            {
+                t = Mathf.Pow(1f - cameraResetSpeed, Time.unscaledDeltaTime);
+            }
+
+            if (targetDist > cameraPanRadius)
+            {
+                t = Mathf.Min(t, cameraPanRadius / targetDist);
+            }
+
+            focusPoint = Vector3.Lerp(targetPosition, focusPoint, t);
+        } else
+        {
+            focusPoint = targetPosition;
+        }
+    }
+
+    private void UpdateCameraInput()
+    {
+        if (canMove)
+        {
+            
+            CheckXAndYAxisInversion();
+
+            if (ManualRotation()) {
+
+                // constrain orbit angles
+                cameraOrbitAngle.x = Mathf.Clamp(cameraOrbitAngle.x, cameraMinYAngle, cameraMaxYAngle);
+                if (cameraOrbitAngle.y < 0f)
+                {
+                    cameraOrbitAngle.y += 360f;
+                }
+                else if (cameraOrbitAngle.y >= 360f)
+                {
+                    cameraOrbitAngle.y -= 360f;
+                }
+
+                lookRotation = Quaternion.Euler(cameraOrbitAngle);
+            } else
+            {
+                lookRotation = transform.localRotation;
+            }
+        }
+    }
+
+    private bool ManualRotation()
+    {
+        // Flip x and y values for lookVector so lookVector.x defines the 
+        // vertical orientation along the Y axis (e.g. tilt), and lookVector.y defines the 
+        // horizontal orientation along the Z axis (e.g. pan)
+
+        Vector2 camInput;
+        camInput.x = lookVector.y * cameraSensitivity;
+        camInput.y = lookVector.x * cameraSensitivity;
+
+        // TODO (aoyeola): Make proper right stick deadzone values
+        const float dz = 0.001f;
+        if (camInput.x < -dz || camInput.x > dz || camInput.y < -dz || camInput.y > dz)
+        {
+            cameraOrbitAngle += cameraRotateSpeed * Time.unscaledDeltaTime * camInput;
+            return true;
+        }
+        return false;
+    }
+
+    private void CheckXAndYAxisInversion()
+    {
+        if (invertYAxis)
+        {
+            lookVector.y *= -1;
+        }
+        if (invertXAxis)
+        {
+            lookVector.x *= -1;
+        }
+    }
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,23 +3,44 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using TMPro;
+using System.Security.AccessControl;
+using UnityEngine.Assertions.Must;
+using System.ComponentModel;
 
+[RequireComponent(typeof(Camera))]
 public class PlayerController : MonoBehaviour
 {
-    public float speed = 0;
-    private Rigidbody rb;
+
     public Camera cam;
+    public TextMeshProUGUI brachesCount;
+   
+    public int branches = 0;
+    public float rotationSmoothing = 0.05f;
+    public float rotationSmoothingVelocity;
+    public float speed = 10;
+
+
+    private Rigidbody rb;
+    //private Vector3 playerMovement;
+    
+    private bool canMove = true;
     private float movementX;
     private float movementY;
-    private Quaternion camRotation;
-    public int branches = 0;
-    public TextMeshProUGUI brachesCount;
+
+
+    private void Awake()
+    {
+        if (cam.Equals(null))
+        {
+            Debug.LogError("Camera not found.");
+        }
+    }
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody>();
-        camRotation = cam.transform.rotation;
+        //playerMovement = Vector3.zero;
     }
 
     void OnMove(InputValue movementValue)
@@ -27,6 +48,12 @@ public class PlayerController : MonoBehaviour
         Vector2 movementVector = movementValue.Get<Vector2>();
         movementX = movementVector.x;
         movementY = movementVector.y;
+    }
+
+    void OnLook(InputValue lookValue)
+    {
+        //Debug.Log(lookValue.Get<Vector2>());
+        cam.GetComponent<CameraController>().lookVector = lookValue.Get<Vector2>();
     }
 
     public void OnCollisionEnter(Collision other)
@@ -60,14 +87,76 @@ public class PlayerController : MonoBehaviour
     void FixedUpdate()
     {
         Vector3 movement = new Vector3(movementX, 0.0f, movementY);
-        rb.AddForce(movement * speed);
-        transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(movement), 0.15f);
-        transform.Translate(movement * speed * Time.deltaTime, Space.World);
+        //rb.AddForce(movement * speed);
     }
 
     // Update is called once per frame
     void Update()
     {
         brachesCount.GetComponent<TextMeshProUGUI>().text = "Apples Collected: " + branches;
+
+        if (canMove)
+        {
+            var playerMovement = new Vector3(movementX, 0f, movementY);
+            if (playerMovement.magnitude >= 0.1f)
+            {
+                float targetAngle = Mathf.Atan2(playerMovement.x, playerMovement.z) * Mathf.Rad2Deg + cam.transform.eulerAngles.y;
+                float smoothedRotationAngle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref rotationSmoothingVelocity, rotationSmoothing);
+
+                transform.rotation = Quaternion.Euler(0f, smoothedRotationAngle, 0f);
+                Vector3 movementDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+
+                transform.Translate(movementDir * speed * Time.deltaTime, Space.World);
+            }
+        }        
+    }
+
+    public static bool CheckGroundNear(
+       Vector3 charPos,
+       float jumpableGroundNormalMaxAngle,
+       float rayDepth, //how far down from charPos will we look for ground?
+       float rayOriginOffset, //charPos near bottom of collider, so need a fudge factor up away from there
+       out bool isJumpable
+    )
+    {
+
+        bool ret = false;
+        bool _isJumpable = false;
+
+
+        float totalRayLen = rayOriginOffset + rayDepth;
+
+        Ray ray = new Ray(charPos + Vector3.up * rayOriginOffset, Vector3.down);
+
+        int layerMask = 1 << LayerMask.NameToLayer("Default");
+
+
+        RaycastHit[] hits = Physics.RaycastAll(ray, totalRayLen, layerMask);
+
+        RaycastHit groundHit = new RaycastHit();
+
+        foreach (RaycastHit hit in hits)
+        {
+
+            if (hit.collider.gameObject.CompareTag("ground"))
+            {
+
+                ret = true;
+
+                groundHit = hit;
+
+                _isJumpable = Vector3.Angle(Vector3.up, hit.normal) < jumpableGroundNormalMaxAngle;
+
+                break; //only need to find the ground once
+
+            }
+
+        }
+
+        //Helper.DrawRay(ray, totalRayLen, hits.Length > 0, groundHit, Color.magenta, Color.green);
+
+        isJumpable = _isJumpable;
+
+        return ret;
     }
 }
