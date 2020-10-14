f3c6d01bf7286cbc41995a1e3d454d3ab8c71489
nabe30 <nabe30@gatech.edu>
2020-10-13 21:25:28 -0400

    Fixes a typo

--- Assets/Scripts/AI/NPCAI.cs
+++ Assets/Scripts/AI/NPCAI.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [RequireComponent(typeof(UnityEngine.AI.NavMeshAgent))]
-public class NPCAIw : MonoBehaviour
+public class NPCAI : MonoBehaviour
 {
 
     private Animator anim;
