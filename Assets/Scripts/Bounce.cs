943cb353713d3f26e9a83183f7ef28741dc2c48b
Lisa Walkosz-Migliacio <evilisa@gatech.edu>
2020-11-27 03:56:58 -0600

    made the pumpkins into trampolines

---
+++ Assets/Scripts/Bounce.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bounce : MonoBehaviour
+{
+    void OnCollisionEnter(Collision other)
+    {
+        float original = other.rigidbody.velocity.y;
+        other.rigidbody.AddForce(0, 499.9699999f, 0, ForceMode.Impulse); 
+    }
+}
